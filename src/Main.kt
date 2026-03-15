e5e3533634fc109891920d73e704781925807aab
EmanueliHT <emanueli300501@edu.unipar.br>
2026-03-15 14:40:33 -0300

    oi

---
+++ src/Main.kt
@@ -0,0 +1,27 @@
+import java.lang.Thread.sleep
+
+fun main (){
+    var semafaroAberto =true
+    val t1 =Thread{
+        while(true){
+            semafaroAberto =true
+            println("Semáforo Aberto")
+            sleep(5000)
+            semafaroAberto = false
+            println("Semáfaro Fechado")
+            sleep (10000)
+        }
+    }
+    val t2 = Thread{
+        for (i in 1..10){
+            if(semafaroAberto){
+                println("Carro $i passou")
+            }else {
+                println("Carro $i esperou")
+            }
+            sleep(2000)
+        }
+    }
+    t1.start()
+    t2.start()
+}
